9a766bbbe08c0417640cb2b2b302d3da89cee035
MuhammadAbyaz <m.abyaz681@gmail.com>
2025-05-14 07:44:52 +0500

    fix: change server approach from serverless to server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,23 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
-const server = express();
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-
-  // Enable CORS if needed
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
   });
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.init();
+  await app.listen(process.env.PORT ?? 3000);
 }
-
 bootstrap();
-
-// Export for serverless use
-export default server;
